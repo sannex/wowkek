af3d5c65867265585a500425c73abffc41f7f67b
HowellsJonathan <howells.jonnathan@gmail.com>
2023-11-21 22:59:13 +0000

    Grid items

--- src/components/Talentstuff/Maincard.js
+++ src/components/Talentstuff/Maincard.js
@@ -3,13 +3,30 @@ import './Maincard.css';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import BasicCard from './BasicCard.js';
+import Grid from '@mui/material/Unstable_Grid2';
 
 export default function SimpleContainer() {
   return (
     <React.Fragment>
       <CssBaseline />
-      <Container maxWidth="sm">
-        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }} />
+      <Container maxWidth="lg">
+        <Box sx={{ bgcolor: '#cfe8fc', height: '100vh' }}>
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <BasicCard />
+            </Grid>
+            <Grid item xs={6}>
+              <BasicCard />
+            </Grid>
+            <Grid item xs={6}>
+              <BasicCard />
+            </Grid>
+            <Grid item xs={6}>
+              <BasicCard />
+            </Grid>
+          </Grid>
+        </Box>
       </Container>
     </React.Fragment>
   );
